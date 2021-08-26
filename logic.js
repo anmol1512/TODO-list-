dd935c185cb0d13a3f04bf385c274a463e1e15d6
Anmol Chhetri <anmol1512@gmail.com>
2021-08-27 01:20:55 +0530

    added 1.favicon 2.navbar_sticky 3.backimg_fixed 4.delete_action_in_your_items_EcmaScript

--- logic.js
+++ logic.js
@@ -1,3 +1,20 @@
+function del(ind){
+    let arr=JSON.parse(localStorage.getItem('itemsjson'));
+        arr.splice(ind,1);
+        localStorage.setItem('itemsjson',JSON.stringify(arr));
+        var itemm=``;
+        cou=1;
+        arr.forEach((element,index) => {
+            itemm+=`<tr>
+            <td scope="row">${index+1}</td>
+            <td>${element[0]}</td>
+            <td>${element[1]}</td>
+            <td><button type="submit" class="btn btn-primary del" onclick="del(${index})" >Delete</button></td>  
+            </tr>
+            `
+        });
+        document.getElementById('row').innerHTML=itemm;
+}
 function update(){
     tit=document.getElementById("title").value;
     desc=document.getElementById("desc").value;
@@ -14,11 +31,12 @@ function update(){
     var item=``;
     count=1;
     // document.getElementById('row').innerHTML="";
-    JSON.parse(localStorage.getItem('itemsjson')).forEach(element => {
+    JSON.parse(localStorage.getItem('itemsjson')).forEach((element,index) => {
         item+=`<tr>
-        <th scope="row">${count++}</th>
+        <td scope="row">${index+1}</td>
         <td>${element[0]}</td>
         <td>${element[1]}</td>
+        <td><button type="submit" class="btn btn-primary" onclick="del(${index})" >Delete</button></td>  
         </tr>
         `
         
@@ -27,10 +45,51 @@ function update(){
     document.getElementById('title').value="";
     document.getElementById('desc').value="";
 }
-
+document.getElementById('title').value='';
+document.getElementById('desc').value='';
+if(localStorage.getItem('itemsjson')!=null){
+    var it=``;
+    // document.getElementById('row').innerHTML="";
+    JSON.parse(localStorage.getItem('itemsjson')).forEach((element,index) => {
+        it+=`<tr>
+        <td scope="row">${index+1}</td>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td>
+        <td><button type="submit" class="btn btn-primary" onclick="del(${index})" >Delete</button></td>  
+        </tr>
+        `
+        
+    });
+    document.getElementById('row').innerHTML=it;
+}
 btn=document.getElementById("add");
 btn.addEventListener("click",update);
-window.addEventListener('load',()=>{
-    document.getElementById('title').value='';
-    document.getElementById('desc').value='';
-})
+
+
+// d=document.getElementsByClassName("del");
+// for(var i=0;i<del.length;i++){
+//     del[i].addEventListener("click",()=>{
+//         let arr=JSON.parse(localStorage.getItem('itemsjson'));
+//         arr.splice(i,1);
+//         localStorage.setItem('itemsjson',JSON.stringify(arr));
+//         var itemm=``;
+//         cou=1;
+//         arr.forEach(element => {
+//             itemm+=`<tr>
+//             <td scope="row">${cou++}</td>
+//             <td>${element[0]}</td>
+//             <td>${element[1]}</td>
+//             <td><button type="submit" class="btn btn-primary del">Delete</button></td>  
+//             </tr>
+//             `  
+//         });
+//         document.getElementById('row').innerHTML=item;
+
+//     })
+
+// }
+
+// window.addEventListener('load',()=>{
+//     document.getElementById('title').value='';
+//     document.getElementById('desc').value='';
+// })
